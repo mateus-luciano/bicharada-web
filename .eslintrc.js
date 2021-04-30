3a90865ec44477d7eed1f9a0d538dd9f8200843e
Mateus Luciano <teeusdm@gmail.com>
2021-04-30 09:41:52 -0300

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'no-unused-vars': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'no-redeclare': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-fragments': 'off',
+    'keyword-spacing': 'off',
+    'arrow-body-style': 'off',
+    'react/destructuring-assignment': 'off',
+  },
+};
