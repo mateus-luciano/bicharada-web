7ef21ce8ac029df194238e49875d1d35bef22813
Mateus Luciano <teeusdm@gmail.com>
2021-04-30 10:47:08 -0300

    updated footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,63 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
+import {
+  Footer,
+  FooterContainer,
+  FooterTextHeading,
+  FooterSubtitle,
+  SocialIcons,
+  SocialIconLink,
+} from './styles';
+
 export default () => {
   return(
-    <h1>Footer</h1>
+    <Footer>
+      <FooterContainer>
+        <FooterTextHeading>
+          Bicharada 2021
+        </FooterTextHeading>
+        <FooterSubtitle>
+          Desenvolvido por Mateus
+        </FooterSubtitle>
+        <SocialIcons>
+          <SocialIconLink
+            href="#"
+            target="_blank"
+            aria-label="Facebook"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook />
+          </SocialIconLink>
+          <SocialIconLink
+            href="#"
+            target="_blank"
+            aria-label="Instagram"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </SocialIconLink>
+          <SocialIconLink
+            href="#"
+            target="_blank"
+            aria-label="Linkedin"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </SocialIconLink>
+          <SocialIconLink
+            href="#"
+            target="_blank"
+            aria-label="Twitter"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter />
+          </SocialIconLink>
+        </SocialIcons>
+      </FooterContainer>
+    </Footer>
   );
 };
--- src/components/Footer/styles.jsx
+++ src/components/Footer/styles.jsx
@@ -1,7 +1,49 @@
 import styled from 'styled-components';
+import { Container } from '../../styles/global';
 
 export const Footer = styled.footer`
-  background: #f0f0f7;
   width: 100%;
   height: 80px;
+  background: #f0f0f7;
+  border-top: 1px solid #aaaa;
+`;
+
+export const FooterContainer = styled(Container)`
+  background: inherit;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 1.2rem;
+`;
+
+export const FooterTextHeading = styled.h2`
+  font-size: 1.3em;
+  color: #000a;
+`;
+
+export const FooterSubtitle = styled.p`
+  font-size: 1em;
+  color: #000;
+`;
+
+export const SocialIcons = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const SocialIconLink = styled.a`
+  color: #341096;
+  font-size: 2em;
+  margin: 0 .1em;
+  transition: filter .2s;
+
+  &:hover {
+    cursor: pointer;
+    filter: brightness(.4);
+  }
 `;
