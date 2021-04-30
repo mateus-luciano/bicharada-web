54df017980cfc72a0ac4eeca0d70ee2581ace15d
Mateus Luciano <teeusdm@gmail.com>
2021-04-30 10:19:38 -0300

    updated header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Button } from '../../styles/global';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavItemBtn,
+  NavBtnLink,
+} from './styles';
+
 export default () => {
-  return(
-    <h1>Header</h1>
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const showButton = () => {
+    if (window.innerHeight <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <IconContext.Provider value={{ color: '#fff' }}>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/" onClick={closeMobileMenu}>
+            <NavIcon />
+            Bicharada
+          </NavLogo>
+          <MobileIcon onClick={handleClick}>
+            { click ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
+          <NavMenu onClick={handleClick} click={click}>
+            <NavItem>
+              <NavLinks to="/">Home</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/">Products</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/">Contact</NavLinks>
+            </NavItem>
+            <NavItemBtn>
+              { button ? (
+                <NavBtnLink to="/sign-up">
+                  <Button primary>SIGN UP</Button>
+                </NavBtnLink>
+              ) : (
+                <NavBtnLink to="/sign-up">
+                  <Button fontBig primary>SIGN UP</Button>
+                </NavBtnLink>
+              ) }
+            </NavItemBtn>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </IconContext.Provider>
   );
 };
