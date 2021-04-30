815654eadd4928aeef19ce57fbc13bdb5d2b401d
Mateus Luciano <teeusdm@gmail.com>
2021-04-30 10:03:59 -0300

    created global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 export default () => (
   <React.Fragment>
+    <GlobalStyles />
     <Routes />
   </React.Fragment>
 );
---
+++ src/styles/global.jsx
@@ -0,0 +1,55 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  root: {
+    --background-color: #f0f0f7;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+
+  html,
+  body {
+    background: var(--background-color);
+  }
+`;
+
+export const Container = styled.div`
+  z-index: 1;
+  width: 100%;
+  max-width: 1300px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 50px;
+  padding-left: 50px;
+
+  @media screen and (max-width: 991px) {
+    padding-right: 30px;
+    padding-left: 30px;
+  }
+`;
+
+export const Button = styled.button`
+  border-radius: 4px;
+  color: #fff;
+  white-space: nowrap;
+  background: ${({ primary }) => (primary ? '#4b59f7' : '#0467fb')};
+  padding: ${({ big }) => (big ? '12px 64px' : '10px 20px')};
+  font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    transition: all .3s ease-out;
+    background: ${({ primary }) => (primary ? '#0467fb' : '#4b59f7')};
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+  }
+`;
