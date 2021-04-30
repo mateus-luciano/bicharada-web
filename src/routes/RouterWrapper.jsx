58fc7dbb37f82bb092d4eab7c0318854680384ed
Mateus Luciano <teeusdm@gmail.com>
2021-04-30 09:59:48 -0300

    created router wrapper

---
+++ src/routes/RouterWrapper.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+// import Layout from '../config/layout';
+
+export default function RouteWrapper({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <Component {...props} />
+      )}
+    />
+  );
+}
