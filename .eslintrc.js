b0c33c814e1ab7a9d17da8f11a235561e86c2b61
Mateus Luciano <teeusdm@gmail.com>
2021-04-30 09:57:12 -0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,16 @@ module.exports = {
     'react',
   ],
   rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'no-unused-vars': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'no-redeclare': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-fragments': 'off',
+    'keyword-spacing': 'off',
+    'arrow-body-style': 'off',
+    'react/destructuring-assignment': 'off',
   },
 };
