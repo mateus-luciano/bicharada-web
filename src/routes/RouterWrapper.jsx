8637b7fa0dde39ddb661ef88d7cf282840f5bad4
Mateus Luciano <teeusdm@gmail.com>
2021-05-01 14:46:29 -0300

    updated router wrapper

--- src/routes/RouterWrapper.jsx
+++ src/routes/RouterWrapper.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Layout from '../config/layout';
 
-export default function RouteWrapper({ component: Component, ...rest }) {
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const user = useSelector((state) => state.user);
+  let signed = false;
+
+  if (user) {
+    signed = true;
+  }
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/login" />;
+  }
+
   return (
     <Route
       {...rest}
