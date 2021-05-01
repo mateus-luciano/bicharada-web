e5dc785bf87004e2d9c8176d9f1c38e4de3cd9e1
Mateus Luciano <teeusdm@gmail.com>
2021-05-01 14:48:08 -0300

    add provider and persistGate in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 export default () => (
-  <React.Fragment>
-    <GlobalStyles />
-    <Routes />
-  </React.Fragment>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <GlobalStyles />
+      <Routes />
+    </PersistGate>
+  </Provider>
 );
