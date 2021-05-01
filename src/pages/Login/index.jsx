3c715a59145f266a72d277213e2c6ac261c57f4d
Mateus Luciano <teeusdm@gmail.com>
2021-05-01 15:29:24 -0300

    updated login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Input, Button } from '../../styles/global';
 
 import LoginImage from '../../assets/images/undraw_Access_account_re_8spm.svg';
 
 import api from '../../services/api';
+
+import * as userActions from '../../store/modules/user/actions';
 
 import {
   LoginContainer,
@@ -18,6 +21,24 @@ import {
 export default () => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
+  const dispatch = useDispatch();
+
+  async function handleStoreLogin(e) {
+    e.preventDefault();
+    console.log('aqui');
+    try {
+      const response = await api.post('/login', {
+        email,
+        password,
+      });
+      console.log(response.data);
+      const isAdmin = response?.data?.user?.admin;
+      dispatch(userActions.login(response.data));
+    } catch (error) {
+      setShowErrorAlert(true);
+    }
+  }
 
   return(
     <LoginContainer>
@@ -35,9 +56,19 @@ export default () => {
       <LoginSection>
         <Form>
           <h2>Login</h2>
-          <Input desk id="email" placeholder="E-mail" />
-          <Input desk id="password" placeholder="Senha" />
-          <Button>
+          <Input
+            desk
+            id="email"
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            desk
+            id="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="button" onClick={handleStoreLogin}>
             Entrar
           </Button>
         </Form>
