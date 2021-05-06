1ba82cd9617672ca0158b71692d0efcdb71be09d
Mateus Luciano <teeusdm@gmail.com>
2021-05-06 20:36:06 -0300

    updated dashbord

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,52 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import Spinner from '../../components/Spinner';
+import AdoptionWrapper from '../../components/AdoptionWrapper';
+
+import api from '../../services/api';
+
+import { DashboardContainer, AdoptionContainer } from './styles';
+
 export default () => {
+  const [loading, setLoading] = useState(false);
+  const [myAdoptions, setMyAdoptions] = useState([]);
+  const userData = useSelector((state) => state?.user);
+
+  async function getUserData() {
+    setLoading(true);
+
+    try {
+      const response = await api.get(`/users/${userData?.user?.uid}`, {
+        headers: { Authorization: `Bearer ${userData?.token}` },
+      });
+      console.log(response.data);
+      setMyAdoptions(response?.data?.adoptions);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return(
-    <div>
-      <h1>Dashbord</h1>
-    </div>
+    <DashboardContainer>
+      <Spinner visible={loading} />
+      <p>Bem vindo</p>
+      { userData.user.name }
+      <h1>Minha adoções</h1>
+      <AdoptionContainer>
+        {myAdoptions.map((adoption) => (
+          <AdoptionWrapper
+            image={adoption.title}
+            title={adoption.title}
+            text={adoption.description}
+            city={adoption.address}
+          />
+        ))}
+      </AdoptionContainer>
+    </DashboardContainer>
   );
 };
---
+++ src/pages/Dashboard/styles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { Container, Button } from '../../styles/global';
+
+export const DashboardContainer = styled(Container)`
+  min-width: 1360px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const AdoptionContainer = styled.div`
+  width: 100%;
+  background: #8b8b8b;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
