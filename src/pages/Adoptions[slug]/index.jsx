16b3f45698891bcb2cb85cc0c8c928d6c54852e9
Mateus Luciano <teeusdm@gmail.com>
2021-05-11 22:34:20 -0300

    installed dependencies

--- src/pages/Adoptions[slug]/index.jsx
+++ src/pages/Adoptions[slug]/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -7,6 +8,15 @@ import {
   Collapse,
   Snackbar,
 } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 
@@ -48,6 +58,52 @@ export default () => {
   const [messageSuccessAlert, setMessageSuccessAlert] = useState();
   const [showErrorAlert, setShowErrorAlert] = useState(false);
   const [messageErrorAlert, setMessageErrorAlert] = useState();
+  const [images, setImages] = useState([]);
+
+  const theme = useTheme();
+  const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+  const tutorialSteps = [
+    {
+      label: 'San Francisco – Oakland Bay Bridge, United States',
+      imgPath:
+        'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+    {
+      label: 'Bird',
+      imgPath:
+        'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+    {
+      label: 'Bali, Indonesia',
+      imgPath:
+        'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+    },
+    {
+      label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
+      imgPath:
+        'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+    {
+      label: 'Goč, Serbia',
+      imgPath:
+        'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+  ];
+
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = tutorialSteps.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
 
   const UserIsAdmin = () => {
     setAdmin(false);
@@ -87,7 +143,7 @@ export default () => {
         setShowAlert(true);
         setTimeout(() => {
           history.push('/adoptions');
-        }, 3000);
+        }, 2000);
       }
       setLoading(false);
       if (response?.data?.message) {
@@ -127,7 +183,9 @@ export default () => {
     setAdoption('');
     try {
       const response = await api.get(`/adoptions/${slug}`);
+      setImages(response?.data?.attachments);
       await getUserData(response?.data?.data?.user_uid);
+      console.log(response?.data?.attachments);
       setAdoption(response?.data?.data);
       setLoading(false);
     } catch (error) {
@@ -152,14 +210,50 @@ export default () => {
   useEffect(() => {
     findAdoption();
   }, []);
-
   return(
     <AdoptionContainer>
       <Spinner visible={loading} />
       { adoption ? (
         <AdoptionWrapper>
           <ImageWrapper>
-            <Image src={notFoundSvg} alt={adoption.title} />
+            {/* {images.map((image) => {
+              <Image src={image.url} alt={adoption.title} />;
+            })} */}
+            <Paper square elevation={0}>
+              <Typography>{tutorialSteps[activeStep].label}</Typography>
+            </Paper>
+            <AutoPlaySwipeableViews
+              axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+              index={activeStep}
+              onChangeIndex={handleStepChange}
+              enableMouseEvents
+            >
+              {tutorialSteps.map((step, index) => (
+                <div key={step.label}>
+                  {Math.abs(activeStep - index) <= 2 ? (
+                    <Image src={step.imgPath} alt={step.label} />
+                  ) : null}
+                </div>
+              ))}
+            </AutoPlaySwipeableViews>
+            <MobileStepper
+              steps={maxSteps}
+              position="static"
+              variant="text"
+              activeStep={activeStep}
+              nextButton={(
+                <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                  Next
+                  {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                </Button>
+        )}
+              backButton={(
+                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                  {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                  Back
+                </Button>
+        )}
+            />
             <SpanAddress>
               {adoption.address}
             </SpanAddress>
